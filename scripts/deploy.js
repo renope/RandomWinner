2906cdeeacf8ccd18e61e7fc7116bd8f309f0b13
Renope <r.nouripoor@gmail.com>
2023-05-01 00:24:54 +0330

    deployed on mumbai

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,17 +7,17 @@ const { verify } = require("./utils/verifier.js")
     const ticketPrice = (10 ** 16).toString()  // 0.01 ETH
     const duration = (24 * 60 * 60).toString() // 24 hours
       
-    // deploy
-    const Cont = await ethers.getContractFactory("RandomWinner");
-    const rw = await Cont.deploy(
-      ticketPrice,
-      duration
-    );
+    // // deploy
+    // const Cont = await ethers.getContractFactory("RandomWinner");
+    // const rw = await Cont.deploy(
+    //   ticketPrice,
+    //   duration
+    // );
 
-    console.log("Cont address : ", rw.address);
+    // console.log("Cont address : ", rw.address);
 
     await delay(5000);
-    await verify(rw.address, [ticketPrice, duration])
+    await verify("0x4F9e21f45abf483d0F57e7B6163D0192479eBAa8", [ticketPrice, duration])
   }
 
   deploy();
